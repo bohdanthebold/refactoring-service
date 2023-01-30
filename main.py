2988c51a8c542ef8ca1fee55f47d7ee2b8bf9122
Dmytro Martyniuk <dmytro.martyniuk@intellias.com>
2023-01-30 15:39:01 +0200

    Applied formatting and naming style

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+import datetime
+
+from math import ceil, floor
 from typing import List
-from math import *
+from pydantic import BaseModel
+from fastapi import FastAPI
+
 
 app = FastAPI()
 
 
 class DB:
     # Fake db
-    def get_userItems(self):
-        return [{'title': "1 item", "is_published": True},
-    {'title': "2 item", "is_published": False},
-        {'title': "3 item", "is_published": False},
-            {'title': "4 item", "is_published": True},
+    def get_user_items(self):
+        return [
+            {"title": "1 item", "is_published": True},
+            {"title": "2 item", "is_published": False},
+            {"title": "3 item", "is_published": False},
+            {"title": "4 item", "is_published": True},
         ]
 
 
@@ -29,44 +33,49 @@ class ItemList(BaseModel):
 
 @app.get("/get-service-name")
 async def service():
-    f = open("service.txt", "r")
-    f = f.read()
-    return {"result": f}
+    file = open("service.txt", "r")
+    line = file.read()
+    return {"result": line}
 
 
 @app.get("/magic-day/")
 async def magic_day():
-    import datetime
-    if datetime.datetime.now().weekday() == 0:return {"result": True}
-    if datetime.datetime.now().weekday() == 2:return {"result": True}
-    if datetime.datetime.now().weekday() == 4:return {"result": True}
-    if datetime.datetime.now().weekday() == 6:return {"result": True}
+    if datetime.datetime.now().weekday() == 0:
+        return {"result": True}
+    if datetime.datetime.now().weekday() == 2:
+        return {"result": True}
+    if datetime.datetime.now().weekday() == 4:
+        return {"result": True}
+    if datetime.datetime.now().weekday() == 6:
+        return {"result": True}
     return {"result": False}
 
 
-def weekend(y):
-    if y == 5:
+def weekend(weekday):
+    if weekday == 5:
         return True
     else:
-        if y == 6:
+        if weekday == 6:
             return True
         else:
             return False
 
 
 @app.get("/is-weekends")
-async def is_weekend():
-    import datetime
-    y = weekend(datetime.datetime.now().weekday())
+async def is_today_weekend():
+
+    is_weekend = weekend(datetime.datetime.now().weekday())
     # Check if true
-    if y:
+    if is_weekend:
         return {"result": True}
-    if not y:
+    if not is_weekend:
         return {"result": False}
 
 
 @app.get("/is-valid-user")
-async def is_valid_user(user: str = "Ahmed", age: int = 30, job: str = "data scientist"):
+async def is_valid_user(
+    user: str = "Ahmed", age: int = 30, job: str = "data scientist"
+):
     if age >= 30:
         if user == "Ahmed":
             if job == "data scientist":
@@ -90,43 +99,47 @@ async def capitalize_string(s: str = "some text", is_uppercase: bool = True):
 
 @app.post("/capitalize-list")
 async def capitalize_list(s: str = "first,second,third"):
-    l = s.split(",")
-    print(l)
+    input_list = s.split(",")
+    print(input_list)
     # iterate over list
     i = 0
     while True:
-        l[i] = l[i].upper()
-        if i >= len(l) - 1:
+        input_list[i] = input_list[i].upper()
+        if i >= len(input_list) - 1:
             break
         i = i + 1
-    return {"result": l}
+    return {"result": input_list}
 
 
-class sendEmailSendgrid():
+class SendEmailSendgrid:
     api_key = "api:sendgrid"
 
     def send(self, message):
         # Some implementation
-        print(f"Sending email via Sendgrid provider with content: {message}. api key: {self.api_key}")
-        pass
+        print(
+            f"Sending email via Sendgrid provider with content: {message}. api key: {self.api_key}"
+        )
 
 
-class sendEmailMailgun(sendEmailSendgrid):
+class SendEmailMailgun(SendEmailSendgrid):
     api_key = "api:mailgun"
 
     def send(self, message):
         # Some implementation
-        print(f"Sending email via Mailgun provider with content: {message}. api key: {self.api_key}")
-        pass
+        print(
+            f"Sending email via Mailgun provider with content: {message}. api key: {self.api_key}"
+        )
 
 
 @app.post("/send-email")
 async def send_email(provider: str = "mailgun", m: str = "message"):
+    message = m
     if provider == "mailgun":
-        sendEmailMailgun().send(message=m)
+        SendEmailMailgun().send(message=message)
     if provider == "sendgrid":
-        sendEmailSendgrid().send(message=m)
+        SendEmailSendgrid().send(message=message)
     return {"ok": True}
+
 
 @app.get("/get-user-id")
 async def exc(username: str = "Paul"):
@@ -139,58 +152,58 @@ async def exc(username: str = "Paul"):
     return {"message": "User not found"}
 
 
-def Filter_Items(is_published, it):
+def filter_items(is_published, items_list):
     if is_published:
-        return list(filter(lambda x: x["is_published"], it))
+        return list(filter(lambda x: x["is_published"], items_list))
     else:
-        return list(filter(lambda x: not x["is_published"], it))
+        return list(filter(lambda x: not x["is_published"], items_list))
 
 
 @app.get("/items")
 async def items():
-    return {"response": Filter_Items(True, db.get_userItems())}
+    return {"response": filter_items(True, db.get_user_items())}
 
 
 @app.get("/key/{key_id}")
 async def key(key_id: int = 2):
     array = ["key1", "key2", "key3", "key4"]
-    for i in range(len(array)):
-        if i + 1 == key_id:
-            return {"result": array[i]}
+    for item_id in range(len(array)):
+        if item_id + 1 == key_id:
+            return {"result": array[item_id]}
 
 
 @app.get("/number-in-both-lists/{key_id}")
 async def number_in_both_lists(key_id: int = 3):
     array1 = [1, 2, 3, 4]
     array2 = [3, 4, 5, 6]
-    for i1 in array1:
-        for i2 in array2:
-            if i1 == key_id and i2 == key_id:
+    for item1 in array1:
+        for item2 in array2:
+            if item1 == key_id and item2 == key_id:
                 return {"result": True}
     return {"result": False}
 
 
-def Add_ID(book_id, storage=[]):
+def add_id(book_id, storage=[]):
     storage.append(book_id)
     return storage
 
 
 @app.post("/add-id")
 async def add_id_view(new_id: int = 1):
-    return {"result": Add_ID(new_id)}
+    return {"result": add_id(new_id)}
 
 
 @app.get("/round")
 async def round_number(number: float = 1.5, to_ceil: bool = True):
     if to_ceil:
-        x = ceil(number)
+        result_value = ceil(number)
     else:
-        x = floor(number)
-    return {"result": x}
+        result_value = floor(number)
+    return {"result": result_value}
 
 
 @app.post("/multiply")
-async def MULtiply(first_number: str = '3', second_number: str = "4"):
+async def multiply(first_number: str = "3", second_number: str = "4"):
     # NOTE: Try inserting this string "exec('import os; result = os.system("touch hack.txt")')#" as a first_number 😉
-    result = float(first_number) * float(second_number)
-    return {"result": result}
+    result_value = float(first_number) * float(second_number)
+    return {"result": result_value}
