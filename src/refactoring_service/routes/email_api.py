9610cb76f452ebcac5504907d66d74e90aa728c6
Dmytro Martyniuk <dmytro.martyniuk@intellias.com>
2023-01-30 19:42:32 +0200

    Moved email logic, applied proper class extension and factory method

--- src/refactoring_service/routes/email_api.py
+++ src/refactoring_service/routes/email_api.py
@@ -1,33 +1,12 @@
 from fastapi import APIRouter
+from services.email_service import EmailService
 
 router = APIRouter()
 
-
-class SendEmailSendgrid:
-    api_key = "api:sendgrid"
-
-    def send(self, message):
-        # Some implementation
-        print(
-            f"Sending email via Sendgrid provider with content: {message}. api key: {self.api_key}"
-        )
-
-
-class SendEmailMailgun(SendEmailSendgrid):
-    api_key = "api:mailgun"
-
-    def send(self, message):
-        # Some implementation
-        print(
-            f"Sending email via Mailgun provider with content: {message}. api key: {self.api_key}"
-        )
+email_service = EmailService()
 
 
 @router.post("/send-email")
 async def send_email(provider: str = "mailgun", m: str = "message"):
-    message = m
-    if provider == "mailgun":
-        SendEmailMailgun().send(message=message)
-    if provider == "sendgrid":
-        SendEmailSendgrid().send(message=message)
-    return {"ok": True}
+    result = email_service.send_email(provider, message=m)
+    return {"ok": result}
